770f9681bea58c4a612f17a142b449b045f212fa
Shun <ter0123540@gmail.com>
2023-02-27 19:50:21 +0800

    feat: add global font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
 import type { AppProps } from 'next/app';
 import '../reset.css';
+import { Caveat } from '@next/font/google';
+
+const caveat = Caveat({
+  subsets: ['latin'],
+  weight: ['400'],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={caveat.className}>
+      <Component {...pageProps} />;
+    </main>
+  );
 }
